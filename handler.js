95b17f3627084f60dbf537efef50b4799790ce3a
Steve Adams <stevezau@gmail.com>
2017-06-26 21:40:19 +1000

    move schedule to own repo

--- handler.js
+++ handler.js
@@ -3,7 +3,6 @@ import fetch from 'node-fetch'
 import moment from 'moment-timezone'
 import consoleTS from 'console-stamp'
 import calcReport from './functions/calcReport'
-import schedule from './functions/schedule'
 
 process.env.TZ = 'UTC'
 moment.tz.setDefault('UTC')
@@ -11,4 +10,3 @@ global.fetch = fetch
 consoleTS(console, 'HH:MM:ss.l')
 
 exports.calcReport = calcReport
-exports.schedule = schedule
