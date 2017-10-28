063fbec4385f719ab774141f986638037a3e4c8d
Steve Adams <stevezau@gmail.com>
2017-10-28 13:26:46 +1100

    add batch script

---
+++ batch_process.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+stores=( bondi balmain avalon brighton )
+fn=$1
+type=$2
+from=$3
+to=$4
+
+for store in "${stores[@]}"
+do
+    echo "Running ${fn} for ${store}"
+   node -r babel-register /usr/local/bin/sls invoke local -f ${fn} --data "{\"store\": \"${store}\",\"type\": \"${type}\", \"from\": \"${from}\",\"to\": \"${to}\"}"
+done
